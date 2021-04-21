60f40b2623b14aea96c7c1af66b15b0082857f72
Ugochukwu <maluugochukwu@gmail.com>
2021-04-21 11:30:30 +0100

    First commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'ugo/datatable',
+  ),
+  'versions' => 
+  array (
+    'ugo/datatable' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'ugo/simlpeorm' => 
+    array (
+      'pretty_version' => '1.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6494ea570747140e75e4054b1a5c3898def68131',
+    ),
+  ),
+);
